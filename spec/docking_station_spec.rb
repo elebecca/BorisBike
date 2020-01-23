f43899b2c6a0042b5f664dcbcaf4ec1a1d6fa690
Student <student@Makerss-MacBook-Air.local>
2020-01-23 15:46:50 +0000

    step 14

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,26 +1,32 @@
-# In spec/docking_station_spec.rb
 require 'docking_station'
-docking_station = DockingStation.new
-bike = Bike.new
 
-describe DockingStation do 
-    it "responds to release bike" do
-      expect(docking_station).to respond_to(:release_bike)
-    end
-    it "docks a bike at a docking station" do
-        expect(subject.dock(bike)).to eq bike
-    end
-    it { is_expected.to respond_to(:bike)}
-    it 'returns docked bikes' do 
+
+describe DockingStation do
+    it { is_expected.to respond_to(:release_bike) }
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'docks something' do
         bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
+        expect(subject.dock(bike)).to eq [bike]
     end
-    describe '#release bike' do
-        it "raises an error when no bikes are avaliable" do
-            expect{subject.release_bike}.to raise_error "No bikes left"
+
+    describe '#dock' do
+        it 'raises an error when full' do
+            20.times { subject.dock(Bike.new) }
+            expect { subject.dock Bike.new }.to raise_error('Docking station full')
+        end
+    end
+
+    describe '#release_bike' do
+        it 'releases a bike' do
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq bike
+        end
+            
+        it 'raises an error when there are no bikes available' do
+            expect { subject.release_bike }.to raise_error('No bikes available')
         end
     end
 end
-
-
